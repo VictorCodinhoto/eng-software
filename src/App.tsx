e3675e001c0da4eede4bc364a6aa72cde3b37af5
VictorCodinhoto <133376215+VictorCodinhoto@users.noreply.github.com>
2024-08-23 10:49:58 -0300

    Starting with id get

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Interface } from 'readline';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Tela 1</h1>
+        <h1>Digite números inteiros!</h1>
         <div>
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
+          <input type="number" id='num1'/>
+          <input type="number" id='num2'/>
+          <input type="number" id='num3'/>
+          <input type="number" id='num4'/>
+          <input type="number" id='num5'/>
+          <input type="number" id='num6'/>
+          <input type="number" id='num7'/>
+          <input type="number" id='num8'/>
         </div>
         <button>Enviar</button>
       </header>
