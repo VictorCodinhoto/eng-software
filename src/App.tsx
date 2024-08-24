bf49942bec83300746fa8aecd9fcc814be5dbf99
VictorCodinhoto <133376215+VictorCodinhoto@users.noreply.github.com>
2024-08-24 12:48:21 -0300

    First Page Done!

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Interface } from 'readline';
+import { ListFormat } from 'typescript';
 
 function App() {
+    const [valor1,setvalor1] = useState('');
+    const [valor2,setvalor2] = useState('');
+    const [valor3,setvalor3] = useState('');
+    const [valor4,setvalor4] = useState('');
+    const [valor5,setvalor5] = useState('');
+    const [valor6,setvalor6] = useState('');
+    const [valor7,setvalor7] = useState('');
+    const [valor8,setvalor8] = useState('');
+    var [lista,setLista] = useState<number[]>([]);
+
   
+    useEffect(() => {
+      if (lista.length > 0) {
+        for (let i = 0; i < lista.length; i++) {
+          if (lista[i] == 0) {
+            alert("Há Campos nulos!");
+            break;
+          }
+          
+          if(i == lista.length -1){
+            alert(lista.join(', '));
+          }
+        }
+      }
+    }, [lista]); 
+  function setIntoListAndOrganize(){
+    const listaDesordenada = [
+      Number(valor1), Number(valor2), Number(valor3), Number(valor4),
+      Number(valor5), Number(valor6), Number(valor7), Number(valor8)
+  ];
+  setLista(bubbleSort(listaDesordenada));
+    
+
+  }
+  function bubbleSort(array: any) {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+          if (array[i] > array[j]) {
+              let aux = array[i];
+              array[i] = array[j];
+              array[j] = aux;
+          }
+      }
+  }
+  return array;
+}
   return (
     <div className="App">
       <header className="App-header">
         <h1>Digite números inteiros!</h1>
         <div>
-          <input type="number" id='num1'/>
-          <input type="number" id='num2'/>
-          <input type="number" id='num3'/>
-          <input type="number" id='num4'/>
-          <input type="number" id='num5'/>
-          <input type="number" id='num6'/>
-          <input type="number" id='num7'/>
-          <input type="number" id='num8'/>
+          <input type="number" value={valor1} onChange={(event) => setvalor1(event.target.value)}/>
+          <input type="number" value={valor2} onChange={(event) => setvalor2(event.target.value)}/>
+          <input type="number" value={valor3} onChange={(event) => setvalor3(event.target.value)}/>
+          <input type="number" value={valor4} onChange={(event) => setvalor4(event.target.value)}/>
+          <input type="number" value={valor5} onChange={(event) => setvalor5(event.target.value)}/>
+          <input type="number" value={valor6} onChange={(event) => setvalor6(event.target.value)}/>
+          <input type="number" value={valor7} onChange={(event) => setvalor7(event.target.value)}/>
+          <input type="number" value={valor8} onChange={(event) => setvalor8(event.target.value)}/>
         </div>
-        <button>Enviar</button>
+        <button onClick={setIntoListAndOrganize}>Enviar</button>
       </header>
     </div>
   );
