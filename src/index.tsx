52208dd8d569c95b7c0f42d1f850d84ceb725d51
VictorCodinhoto <133376215+VictorCodinhoto@users.noreply.github.com>
2024-08-28 11:10:36 -0300

    Pastas organizadas

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import SegundaPagina from './SegundaTela';
+import App from './Pages/App';
+import SegundaPagina from './Pages/SegundaTela';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import TerceiraTela from './TerceiraTela';
+import TerceiraTela from './Pages/TerceiraTela';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
